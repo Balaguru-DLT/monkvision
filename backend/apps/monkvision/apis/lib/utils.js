1ea666555cd0e79d214fe16dc5a6357ecd9bac77
Balaguru-DLT <balaguru.g@deeplogictech.com>
2025-02-17 17:16:01 +0530

    Incremental DB read

--- backend/apps/monkvision/apis/lib/utils.js
+++ backend/apps/monkvision/apis/lib/utils.js
@@ -4,6 +4,8 @@
  * (C) 2020 TekMonks. All rights reserved.
  */
 
+const db = require(`${APP_CONSTANTS.LIB_DIR}/db.js`);
+
 /**
  * Returns SQLite date from ISO date
  * @param {string} date Date in ISO format
@@ -29,3 +31,47 @@ exports.fromSQLiteToUTCOrLocalTime = function(sqliteDate, notUTC) {
 exports.getTimeRangeForSQLite = (range) => {
     return {from: exports.fromISOToSQLite(range.from), to: exports.fromISOToSQLite(range.to)}
 }
+
+/**
+ * Fetches data from the database and caches it if necessary.
+ * @param {string} queryFunc - The database query function name.
+ * @param {string} id -  Log ID Unique identifier.
+ * @param {Object} timeRange - Time range (optional).
+ * @param {...any} extraParams - Additional parameters to pass to the query function.
+ * @returns  Returns an array of fetched or cached data.
+ */
+const cache = {}; 
+
+async function fetchAndCache(id, timeRange, queryFunc, ...extraParams) {
+    const rows = await db[queryFunc](id, timeRange, ...extraParams);
+    cache[id].data.push(...rows);
+    return rows;
+}
+
+async function fetchIncrementalAndCache(id, range, queryFunc, prepend = false, ...extraParams) {
+    const rows = await db[queryFunc](id, range, ...extraParams);
+    if (prepend) cache[id].data.unshift(...rows);
+    else cache[id].data.push(...rows);
+    return rows;
+}
+
+exports.fetchDBData = async function (queryFunc, id, timeRange, ...extraParams) {
+    if (!timeRange) return db[queryFunc](id, ...extraParams);
+    if (!cache[id]) cache[id] = { data: [], lastRange: null };
+    const { lastRange } = cache[id];
+
+    if (!lastRange || (timeRange.from < lastRange.from && timeRange.to > lastRange.to) ) {
+        cache[id].data = await fetchAndCache(id, timeRange, queryFunc, ...extraParams);
+        cache[id].lastRange = { ...timeRange };
+    } else if (timeRange.to > lastRange.to && timeRange.from === lastRange.from) {
+        await fetchIncrementalAndCache(id, { from: lastRange.to, to: timeRange.to }, queryFunc, false, ...extraParams);
+        cache[id].lastRange.to = timeRange.to;
+    } else if (timeRange.from < lastRange.from && timeRange.to <= lastRange.to) {
+        await fetchIncrementalAndCache(id, { from: timeRange.from, to: lastRange.from }, queryFunc, true, ...extraParams);
+        cache[id].lastRange.from = timeRange.from;
+    }else if (timeRange.from >= lastRange.from && timeRange.to <= lastRange.to) {
+        return cache[id].data.filter(row => row.timestamp >= timeRange.from && row.timestamp <= timeRange.to);
+    }
+
+    return cache[id].data.filter(row => row.timestamp >= timeRange.from && row.timestamp <= timeRange.to);
+};
